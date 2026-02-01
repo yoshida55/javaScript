ec030eecf4c1bb652632b9db9db5cb9a4f52533a
github user name <sensha5172@gmail.com>
2026-02-01 13:47:13 +0900

    aaa

--- js/script2.js
+++ js/script2.js
@@ -1,32 +1,44 @@
-console.log("script2 loaded");
-debugger;
-// 配列　７．５、９，　３
-// 元のの番号（配列）
-originalNumbers = [3, 2, 1];
-
-// 昇順後の結果（配列）
-let lineUPnumbers = [];
-
-lineUpNum(originalNumbers);
-
-//　もとの番号をうけとって（7.5.9.3）
-function lineUpNum(originalNumbers) {
-  // もとの番号の長さを取得する（元の昇順する目的の配列）（4つ）
-  let length = originalNumbers.length;
-
-  // target: 確定させたい位置（4つまで、targetを）
-  for (let target = 0; target < length; target++) {
-    // compare: 比較相手（ターゲットの次の数字から最後まで）
-    for (let compare = target + 1; compare < length; compare++) {
-      // originalNumbers(もとの)、
-      if (originalNumbers[target] > originalNumbers[compare]) {
-        // 入れ替え作業
-        let storage = originalNumbers[target];
-        originalNumbers[target] = originalNumbers[compare];
-        originalNumbers[compare] = storage;
+function sortArrayAsc(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      // もし左の数値と右の数値が左の数値が大きい場合、入れ替えを行う
+      if (arr[j] > arr[j + 1]) {
+        let saveBox = arr[j];
+        // 入れ替え。大きいデータを右に移動
+        arr[j] = arr[j + 1];
+        arr[j + 1] = saveBox;
       }
     }
   }
+  return arr;
 }
 
-console.log("配列昇順結果");
+console.log("配列昇順結果１：" + sortArrayAsc([9, 5, 3, 2, 1]));
+
+console.log("ーーーーーーーーーーーーーーーーーーーーーー【58-５別解】にゃんをつける " + "\n" + "ーーーーーーーーーーーーーーーーー-");
+
+function addNyan(words) {
+  // 「。」を「にゃん」におきかえる
+  return (words = words.replace(/。/g, "にゃん"));
+}
+
+console.log(addNyan("こんにちは。お元気ですか。"));
+
+console.log("ーーーーーーーーーーーーーーーーーーーーーー【66-1】DOM課題➀　１" + "\n" + "ーーーーーーーーーーーーーーーーー-");
+const btnRed = document.getElementById("colorChangeBtnRed");
+const btnBlue = document.getElementById("colorChangeBtnBlue");
+const p1 = document.getElementById("box");
+
+btnBlue.addEventListener("click", function () {
+  p1.style.backgroundColor = "blue";
+  console.log("boxがみつかりません");
+});
+
+btnRed.addEventListener("click", function () {
+  p1.style.backgroundColor = "red";
+  console.log("boxがみつかりません");
+});
+
+
+
+
