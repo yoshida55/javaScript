44de27d4fbe3076cd3006b7d2337035afbadf54b
github user name <sensha5172@gmail.com>
2026-02-17 17:10:19 +0900

    update

--- js/画像の切替セクション(最新).js
+++ js/画像の切替セクション(最新).js
@@ -1,7 +1,10 @@
+// 下の画像をクリックしたら、上の画像が切り替わるようにするには、JavaScriptを使用してイベントリスナーを追加する必要があります。以下は、基本的な例です。
+$(function () {
+  $(".img").click(function () {
+    // 画像を取得
+    var newSrc = $(this).attr("src");
 
-$(document).ready(function() {
-  $('.thumbnail').click(function() {
-    var newSrc = $(this).attr('src');
-    $('#mainImage').attr('src', newSrc);
+    $(".MainImg").attr("src", newSrc);
+    // 大きな画像の要素を取得
   });
 });
