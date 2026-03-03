5df9e31f9ead16bfe85145e7c61aee874a5ef585
kenzo <sensha5172+copilot2@gmail.com>
2026-03-03 12:36:06 +0900

    update

--- js/画像切り替えセクション_test2.js
+++ js/画像切り替えセクション_test2.js
@@ -4,7 +4,12 @@ $(function () {
   $(".gallery_img").on("click", function () {
     // クリックされたサムネイル画像のsrc属性を取得
     const imgSrc = $(this).attr("src");
-    // メイン画像のsrc属性をクリックされたサムネイル画像のsrc属性に変更
-    $(".gallery_main").attr("src", imgSrc);
+
+    // メイン画像をフェードアウトしてから、src属性を変更し、再度フェードインする
+    $(".gallery_main").fadeOut(1000, function () {
+      console.log("aaa");
+      // メイン画像のsrc属性をクリックされたサムネイル画像のsrc属性に変更
+      $(this).attr("src", imgSrc).fadeIn(300);
+    });
   });
 });
